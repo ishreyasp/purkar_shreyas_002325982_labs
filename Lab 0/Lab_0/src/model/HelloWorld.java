fc19c18c6f2839358d8c421873decc576352888a
Shreyas Purkar <purkarshreyas@gmail.com>
2024-09-14 19:38:30 -0400

    Add HelloWorld.java class to display Hello World string

---
+++ Lab 0/Lab_0/src/model/HelloWorld.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * @author Shreyas Purkar
+ * 
+ * Class to display "Hello, World" string.
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
