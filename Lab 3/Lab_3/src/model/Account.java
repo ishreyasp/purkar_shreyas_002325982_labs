402ab46a547e4fa1cdf58649bac567bfcbaa2964
Shreyas Purkar <purkarshreyas@gmail.com>
2024-09-28 12:29:36 -0400

    Add Account model

---
+++ Lab 3/Lab_3/src/model/Account.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ * Model for Account details
+ * 
+ * @author Shreyas Purkar
+ */
+public class Account {
+    
+    private String accountNumber;
+    private String routingNumber;
+    private String bankName;
+    private int balance;
+
+    //Default constructor
+    public Account() {
+    }
+
+    //Parameterized constructor
+    public Account(String accountNumber, String routingNumber, String bankName, int balance) {
+        this.accountNumber = accountNumber;
+        this.routingNumber = routingNumber;
+        this.bankName = bankName;
+        this.balance = balance;
+    }
+
+    //Getters and Setters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
