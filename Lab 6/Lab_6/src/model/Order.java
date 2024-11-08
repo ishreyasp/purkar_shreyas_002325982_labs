35d5ad64ed764d2563813caf1a99ea8c9341e873
ishreyasp <purkarshreyas@gmail.com>
2024-11-08 18:59:31 -0500

    Add required models

---
+++ Lab 6/Lab_6/src/model/Order.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author purka
+ */
+class Order {
+    ArrayList<OrderItem> orderItemList;
+
+    public Order() {
+        orderItemList = new ArrayList<>();
+    }
+
+    public ArrayList<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(ArrayList<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+    
+    public void addNewOrderItem(Product product, double price, int quantity) {
+        OrderItem orderItem = new OrderItem(product, price, quantity);
+        orderItemList.add(orderItem);
+    }
+    
+}
